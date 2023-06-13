3d1a7eaf0831b976d1dd2deaadc6482a0d4635f4
Marshall Ku <marshall@kakao.com>
2023-06-13 15:10:55 +0900

    Add utils for getting random integer

---
+++ src/utils/random.ts
@@ -0,0 +1,13 @@
+export function getRandomInt(min = 0, max = 1) {
+    const refinedMin = Math.ceil(min);
+    const refinedMax = Math.floor(max);
+    return Math.floor(Math.random() * (refinedMax - refinedMin) + refinedMin);
+}
+
+export function getRandomIntInclusive(min = 0, max = 1) {
+    const refinedMin = Math.ceil(min);
+    const refinedMax = Math.floor(max);
+    return Math.floor(
+        Math.random() * (refinedMax - refinedMin + 1) + refinedMin
+    );
+}
