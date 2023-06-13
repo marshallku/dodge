16d6c7c9eae50980cb0bc879e653f33c5b75a9ff
Marshall Ku <marshall@kakao.com>
2023-06-13 16:50:04 +0900

    Add logics to check collision in Square

--- src/vectors/Square.ts
+++ src/vectors/Square.ts
@@ -2,11 +2,11 @@ import { Canvas } from "../components";
 import { Coordinate, Figure } from "./types";
 
 export default class Square {
-    #x: number;
-    #y: number;
-    #size: number;
-    #lineWidth: number;
-    #color: string;
+    x: number;
+    y: number;
+    size: number;
+    lineWidth: number;
+    color: string;
 
     constructor({
         x = 0,
@@ -15,44 +15,48 @@ export default class Square {
         lineWidth = 1,
         color = "black",
     }: Partial<Figure> = {}) {
-        this.#x = x;
-        this.#y = y;
-        this.#size = size;
-        this.#lineWidth = lineWidth;
-        this.#color = color;
+        this.x = x;
+        this.y = y;
+        this.size = size;
+        this.lineWidth = lineWidth;
+        this.color = color;
     }
 
     render(context: CanvasRenderingContext2D) {
-        const padding = Math.ceil(this.#lineWidth / 2);
+        const padding = Math.ceil(this.lineWidth / 2);
 
         context.beginPath();
-        context.lineWidth = this.#lineWidth;
-        context.strokeStyle = this.#color;
-        context.rect(
-            this.#x + padding,
-            this.#y + padding,
-            this.#size,
-            this.#size
-        );
+        context.lineWidth = this.lineWidth;
+        context.strokeStyle = this.color;
+        context.rect(this.x + padding, this.y + padding, this.size, this.size);
         context.stroke();
     }
 
     update({ x, y }: Coordinate) {
-        this.#x = x;
-        this.#y = y;
+        this.x = x;
+        this.y = y;
     }
 
     move({ x, y }: Coordinate) {
-        this.#x += x;
-        this.#y += y;
+        this.x += x;
+        this.y += y;
     }
 
     getVisibility(canvas: Canvas) {
         return !(
-            this.#x < -this.#size ||
-            canvas.width + this.#size < this.#x ||
-            this.#y < -this.#size ||
-            canvas.height + this.#size < this.#y
+            this.x < -this.size ||
+            canvas.width + this.size < this.x ||
+            this.y < -this.size ||
+            canvas.height + this.size < this.y
+        );
+    }
+
+    checkCollision(square: Square) {
+        return (
+            this.x + this.size >= square.x &&
+            this.x <= square.x + square.size &&
+            this.y + this.size >= square.y &&
+            this.y <= square.y + square.size
         );
     }
 }
